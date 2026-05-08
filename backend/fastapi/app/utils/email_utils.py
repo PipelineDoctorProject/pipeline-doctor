9a801d9e97143e61f33b2e3b9bcee8971699f4c5
Shakkeera <shakkeerawindow@gmail.com>
2026-05-08 14:21:41 +0530

    Add utils from feature/auth

---
+++ backend/fastapi/app/utils/email_utils.py
@@ -0,0 +1,124 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+from app.config.settings import (
+    MAIL_USERNAME,
+    MAIL_PASSWORD,
+    MAIL_FROM,
+)
+
+
+def send_otp_email(email: str, otp: str):
+
+    subject = "PipelineDoctor OTP Verification"
+
+    body = f"""
+    <h2>PipelineDoctor Verification</h2>
+
+    <p>Your OTP is:</p>
+
+    <h1>{otp}</h1>
+
+    <p>This code expires in 10 minutes.</p>
+    """
+
+    msg = MIMEMultipart()
+
+    msg["From"] = MAIL_FROM
+    msg["To"] = email
+    msg["Subject"] = subject
+
+    msg.attach(MIMEText(body, "html"))
+
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+
+        server.starttls()
+
+        server.login(MAIL_USERNAME, MAIL_PASSWORD)
+
+        server.sendmail(
+            MAIL_FROM,
+            email,
+            msg.as_string()
+        )
+
+        server.quit()
+
+        print("OTP email sent")
+
+    except Exception as e:
+        print("Email error:", e)
+
+
+# ==========================================
+# SEND INVITE EMAIL
+# ==========================================
+def send_invite_email(
+    email: str,
+    invite_link: str
+):
+
+    subject = "PipelineDoctor Workspace Invitation"
+
+    body = f"""
+    <h2>You are invited to PipelineDoctor</h2>
+
+    <p>
+        You have been invited to join a workspace.
+    </p>
+
+    <p>
+        Click below to accept invitation:
+    </p>
+
+    <a href="{invite_link}">
+        Accept Invitation
+    </a>
+
+    <br><br>
+
+    <p>
+        If you did not expect this email,
+        you can ignore it.
+    </p>
+    """
+
+    msg = MIMEMultipart()
+
+    msg["From"] = MAIL_FROM
+    msg["To"] = email
+    msg["Subject"] = subject
+
+    msg.attach(
+        MIMEText(body, "html")
+    )
+
+    try:
+
+        server = smtplib.SMTP(
+            "smtp.gmail.com",
+            587
+        )
+
+        server.starttls()
+
+        server.login(
+            MAIL_USERNAME,
+            MAIL_PASSWORD
+        )
+
+        server.sendmail(
+            MAIL_FROM,
+            email,
+            msg.as_string()
+        )
+
+        server.quit()
+
+        print("Invite email sent")
+
+    except Exception as e:
+
+        print("Email error:", e)
