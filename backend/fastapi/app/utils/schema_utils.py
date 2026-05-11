92f2707d38a68f0391985dd35b1f2bfb41b73cb5
Shakkeera <shakkeerawindow@gmail.com>
2026-05-11 10:59:09 +0530

    tenant based table creation

--- backend/fastapi/app/utils/schema_utils.py
+++ backend/fastapi/app/utils/schema_utils.py
@@ -1,16 +1,38 @@
 from sqlalchemy import text
 
+from app.models.tenant_tables import TENANT_MODELS
+
+
 def create_schema(db, schema_name: str):
+
     if not schema_name.isidentifier():
         raise ValueError("Invalid schema name")
 
-    db.execute(text(f'CREATE SCHEMA IF NOT EXISTS "{schema_name}"'))
+    # Create schema
+    db.execute(
+        text(f'CREATE SCHEMA IF NOT EXISTS "{schema_name}"')
+    )
+
+    db.commit()
+
+    # Dynamically create tenant tables
+    for model in TENANT_MODELS:
+
+        model.__table__.schema = schema_name
+
+        model.__table__.create(
+            bind=db.bind,
+            checkfirst=True
+        )
+
     db.commit()
 
 
 def set_schema(db, schema_name: str):
-    # Basic validation (important for safety)
+
     if not schema_name.isidentifier():
         raise ValueError("Invalid schema name")
 
-    db.execute(text(f'SET search_path TO "{schema_name}"'))
+    db.execute(
+        text(f'SET search_path TO "{schema_name}", public')
+    )
